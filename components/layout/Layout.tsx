4a8b8c7a02008fe5d85f606425a2dbd1e82b1b77
Kabiswa Davis <kabiswad@gmail.com>
2023-04-17 22:11:25 +0200

    style the sidebar

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SideBar from './SideBar'
 interface LayoutProps {
     children: React.ReactNode
 }
@@ -8,6 +9,7 @@ const Layout: React.FC<LayoutProps> = ({children}) => {
     <div className='h-screen bg-black'>
         <div className="container h-full mx-auto xl:px-30 max-w-6xl">
             <div className="grid grid-cols-4 h-full">
+                <SideBar/>
                 <div className="col-span-3 lg:col-span-2 border-neutral-800 border-x-[1px]">
                     {children}
                 </div>
---
+++ components/layout/SideBar.tsx
@@ -0,0 +1,47 @@
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+import { BiLogOut } from 'react-icons/bi';
+import SideBarLogo from './SideBarLogo';
+import SideBarItem from './SideBarItem';
+import SideBarTweetButton from './SideBarTweetButton';
+
+const SideBar = () => {
+    const menuItems = [
+        {
+            label: 'Home',
+            href: '/',
+            icon: BsHouseFill
+        },
+        {
+            label: 'Notifications',
+            href: '/notifications',
+            icon: BsBellFill
+        },
+        {
+            label: 'Profile',
+            href: '/user/123',
+            icon: FaUser
+        }
+    ]
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+        <div className="flex flex-col item-end">
+            <div className="space-y-2 lg:w-[230px]">
+                <SideBarLogo />
+                {menuItems.map((item) => (
+                    <SideBarItem 
+                        key={item.href}
+                        href={item.href}
+                        label={item.label}
+                        icon={item.icon}
+                    />
+                ))}
+                <SideBarItem onClick={() => {}} icon={BiLogOut} label='Logout' />
+                <SideBarTweetButton />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SideBar
